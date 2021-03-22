b8dbc8c6dfbfd4387c606106cccae3e8d55d225f
Nicolas-162 <70936967+Nicolas-162@users.noreply.github.com>
2021-03-22 08:37:38 +0100

    Update NoeudDeListe.h

--- NoeudDeListe.h
+++ NoeudDeListe.h
@@ -1,16 +1,30 @@
-#ifndef NOEUDDELISTE_H
-#define NOEUDDELISTE_H
+#ifndef NOEUDDELISTE_HPP
+#define NOEUDDELISTE_HPP
 
 
 class NoeudDeListe
 {
     public:
-        NoeudDeListe();
-        virtual ~NoeudDeListe();
+        
+    NoeudDeListe();
+    virtual ~NoeudDeListe();
+    
+    explicit NoeudDeListe(int donnee);
+    
+    int getDonnee();
+    void setDonnee(int d);
+    
+    NoeudDeListe* getSuivant();
+    void setSuivant(NoeudDeListe* s);
+    
+    void afficher();
 
     protected:
 
     private:
+    int donnee;
+    NoeudDeListe* suivant;
+    
 };
 
-#endif // NOEUDDELISTE_H
+#endif // NOEUDDELISTE_HPP
